e1e696e725bc10b15ade39d1d0afe4dc30ac00f5
David Underhill <dgu@cs.stanford.edu>
2010-08-17 21:48:41 -0400

    implement setting Rate field

---
+++ controllers/ajax/Rate.py
@@ -0,0 +1,34 @@
+import datetime
+import logging
+
+from google.appengine.ext import db
+
+from Action import ActionHandler
+from models.UserCmt import UserCmt, MIN_RATING, MAX_RATING
+
+class Rate(ActionHandler):
+    @staticmethod
+    def get_action_name():
+        return "rate"
+
+    @staticmethod
+    def is_rate_limited():
+        return True
+
+    def handle_action(self, uid, cid, str_rating):
+        # parse the params
+        try:
+            rating = int(str_rating)
+            if rating<MIN_RATING or rating>MAX_RATING:
+                raise ValueError
+        except ValueError:
+            logging.warn('invalid rating: rating=%s from_uid=%s' % (str_rating, uid))
+            return self.error(400)
+
+        cmt = self.get_cmt_from_cid(cid, uid)
+        if not cmt:
+            return
+
+        # update the rating
+        cmt.rating = rating
+        cmt.put()
