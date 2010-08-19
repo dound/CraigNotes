990d6e0f33926cbf88aaded79eb9d78f5ce99b61
David Underhill <dgu@cs.stanford.edu>
2010-08-19 16:52:08 -0400

    customize which buttons show up on the editor

--- offline/js/view.js
+++ offline/js/view.js
@@ -21,6 +21,83 @@ YUI().use('event-base', 'event-key', 'io-base', 'node-base', 'node-style', 'yui2
     var COMMENT_STATES = {}; // maps cid -> (btn, editor)
     var RATINGS = {};        // maps cid -> (rating, star nodes)
 
+	var EDITOR_CFG = {
+		height: '100px',
+		width: '100%',
+		animate: false,
+		dompath: false,
+		focusAtStart: true,
+		autoHeight: true,
+		toolbar: {
+			collapse: false,
+			titlebar: '',
+			draggable: false,
+			buttonType: 'advanced',
+			buttons: [
+				{ group: 'fontstyle', label: 'Font Name and Size',
+					buttons: [
+						{ type: 'select', label: 'Arial', value: 'fontname', disabled: true,
+							menu: [
+								{ text: 'Arial', checked: true },
+								{ text: 'Arial Black' },
+								{ text: 'Comic Sans MS' },
+								{ text: 'Courier New' },
+								{ text: 'Lucida Console' },
+								{ text: 'Tahoma' },
+								{ text: 'Times New Roman' },
+								{ text: 'Trebuchet MS' },
+								{ text: 'Verdana' }
+							]
+						},
+						{ type: 'spin', label: '13', value: 'fontsize', range: [ 9, 75 ], disabled: true }
+					]
+				},
+				{ type: 'separator' },
+				{ group: 'textstyle', label: 'Font Style',
+					buttons: [
+						{ type: 'push', label: 'Bold CTRL + SHIFT + B', value: 'bold' },
+						{ type: 'push', label: 'Italic CTRL + SHIFT + I', value: 'italic' },
+						{ type: 'push', label: 'Underline CTRL + SHIFT + U', value: 'underline' },
+						{ type: 'separator' },
+						{ type: 'color', label: 'Font Color', value: 'forecolor', disabled: true },
+						{ type: 'color', label: 'Background Color', value: 'backcolor', disabled: true },
+						{ type: 'separator' },
+						{ type: 'push', label: 'Remove Formatting', value: 'removeformat', disabled: true },
+						{ type: 'push', label: 'Show/Hide Hidden Elements', value: 'hiddenelements' },
+						{ type: 'separator' },
+						{ type: 'push', label: 'Undo', value: 'undo', disabled: true },
+						{ type: 'push', label: 'Redo', value: 'redo', disabled: true }
+					]
+				},
+				{ type: 'separator' },
+				{ group: 'alignment', label: 'Alignment',
+					buttons: [
+						{ type: 'push', label: 'Align Left CTRL + SHIFT + [', value: 'justifyleft' },
+						{ type: 'push', label: 'Align Center CTRL + SHIFT + |', value: 'justifycenter' },
+						{ type: 'push', label: 'Align Right CTRL + SHIFT + ]', value: 'justifyright' },
+						{ type: 'push', label: 'Justify', value: 'justifyfull' }
+					]
+				},
+				{ type: 'separator' },
+				{ group: 'indentlist', label: 'Indenting/Lists',
+					buttons: [
+						{ type: 'push', label: 'Indent', value: 'indent', disabled: true },
+						{ type: 'push', label: 'Outdent', value: 'outdent', disabled: true },
+						{ type: 'push', label: 'Create an Unordered List', value: 'insertunorderedlist' },
+						{ type: 'push', label: 'Create an Ordered List', value: 'insertorderedlist' }
+					]
+				},
+				{ type: 'separator' },
+				{ group: 'insertitem', label: 'Insert Item',
+					buttons: [
+						{ type: 'push', label: 'HTML Link CTRL + SHIFT + L', value: 'createlink', disabled: true },
+						{ type: 'push', label: 'Insert Image', value: 'insertimage' }
+					]
+				}
+			]
+		}
+	};
+
     /** show the editing pane if it is hidden, otherwise update the cmt with the edit pane text */
     function edit_ad_cmt(cid, btn) {
         var state = COMMENT_STATES[cid];
@@ -57,16 +134,7 @@ YUI().use('event-base', 'event-key', 'io-base', 'node-base', 'node-style', 'yui2
             txt.set('innerHTML', '<textarea id="cmtedit' + cid + '" rows="5" style="width:100%">' + state.old_text + '</textarea>');
 
             (function (btn, cid) {
-				var Dom = Y.YUI2.util.Dom, Event = Y.YUI2.util.Event;
-				var cfg = {
-					height: '100px',
-					width: '100%',
-					animate: false,
-					dompath: false,
-					focusAtStart: true,
-					autoHeight: true
-				};
-				var editor = new Y.YUI2.widget.Editor('cmtedit' + cid, cfg);
+				var editor = new Y.YUI2.widget.Editor('cmtedit' + cid, EDITOR_CFG);
 				state.editor = editor;
 				editor.render();
 
