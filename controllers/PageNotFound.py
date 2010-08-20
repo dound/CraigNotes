f397e125fbfca74b13fcaefa5cbe8a699e83a330
David Underhill <dgu@cs.stanford.edu>
2010-08-19 20:47:54 -0400

    fix a few links

--- controllers/PageNotFound.py
+++ controllers/PageNotFound.py
@@ -10,4 +10,3 @@ class PageNotFound(webapp.RequestHandler):
     def render(handler):
         handler.error(404)
         handler.response.out.write(MakoLoader.render('404.html', request=handler.request))
-        # TODO: make a better 404 page, notify the admin
